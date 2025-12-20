b507ed5a2b86e65729e7f2ca52bbcc5f44951891
Mateusz Jurzyna <jurzyna@gmail.com>
2025-12-20 12:11:12 +0100

    Update site URL in Astro config and add CNAME for custom domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,8 +5,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://mattJurz.github.io',
-  base: '/czarkowice-astro',
+  site: 'https://czarkowice.pl',
   vite: {
     plugins: [tailwindcss()]
   }
