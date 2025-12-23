f22d4960c4dcb6c6b4d36ad32db7b2666a0793f1
Mateusz Jurzyna <jurzyna@gmail.com>
2025-12-23 08:47:40 +0100

    SEO

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,18 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
+import sitemap from '@astrojs/sitemap';
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://czarkowice.pl',
+  site: 'https://www.czarkowice.pl',
+  integrations: [
+    sitemap({
+      changefreq: 'weekly',
+      priority: 0.7,
+      lastmod: new Date(),
+    }),
+  ],
   vite: {
     plugins: [tailwindcss()]
   }
